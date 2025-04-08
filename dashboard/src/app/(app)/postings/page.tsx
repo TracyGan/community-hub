4a32b18ab60b30f467d7089a55681fccc1b40206
TracyGan <tracyygann@gmail.com>
2025-04-08 23:20:20 +0800

    extract out into separate component

--- dashboard/src/app/(app)/postings/page.tsx
+++ dashboard/src/app/(app)/postings/page.tsx
@@ -16,20 +16,19 @@ import { useCallback, useState } from "react";
 import type { TDonation } from "@/app/utils/types";
 import { EditDonationModal } from "@/app/modals/edit-donation-modal";
 import { DeleteDonationModal } from "@/app/modals/delete-donation-modal";
+import PostingsTable from "./postings-table";
 
 export default function PostingsPage() {
-	const { data: donations } = useGetAllUserDonations();
-
 	const [openDonationModal, setOpenDonationModal] = useState(false);
 	const [openDeleteDonationModal, setOpenDeleteDonationModal] = useState(false);
 	const [selectedItem, setSelectedItem] = useState<TDonation | null>(null);
 
-	const editDonation = useCallback((donation: TDonation) => {
+	const editDonation = (donation: TDonation) => {
 		setSelectedItem(donation);
 		setOpenDonationModal(true);
-	}, []);
+	};
 
-	const useDeleteDonation = (donation: TDonation) => {
+	const deleteDonation = (donation: TDonation) => {
 		setSelectedItem(donation);
 		setOpenDeleteDonationModal(true);
 	};
@@ -41,60 +40,10 @@ export default function PostingsPage() {
 				<div className="text-xl font-semibold">
 					View Donation Postings
 					<div className="py-3">
-						<Table>
-							<TableHeader>
-								<TableRow>
-									<TableHead className="w-[100px] font-semibold">
-										Title
-									</TableHead>
-									<TableHead className="font-semibold">Status</TableHead>
-									<TableHead className="font-semibold">Category</TableHead>
-									<TableHead className="font-semibold">Description</TableHead>
-									<TableHead className="font-semibold">
-										Pickup Location
-									</TableHead>
-									<TableHead className="font-semibold">Pickup Time</TableHead>
-								</TableRow>
-							</TableHeader>
-							<TableBody>
-								{donations?.map((donation) => {
-									return (
-										<TableRow key={donation.id}>
-											<TableCell className="font-medium">
-												{donation.title}
-											</TableCell>
-											<TableCell className="font-medium">
-												{donation.status}
-											</TableCell>
-											<TableCell className="font-medium">
-												{donation.category}
-											</TableCell>
-											<TableCell className="font-medium">
-												{donation.description}
-											</TableCell>
-											<TableCell className="font-medium">
-												{donation.location}
-											</TableCell>
-											<TableCell className="font-medium">
-												{convertDateToString(donation.time)}
-											</TableCell>
-											<TableCell className="font-medium">
-												<span className="flex flex-row">
-													{donation.status !== "RETRIEVED" && (
-														<Icons.PencilIcon
-															onClick={() => editDonation(donation)}
-														/>
-													)}
-													<Icons.TrashIcon
-														onClick={() => useDeleteDonation(donation)}
-													/>
-												</span>
-											</TableCell>
-										</TableRow>
-									);
-								})}
-							</TableBody>
-						</Table>
+						<PostingsTable
+							editDonation={editDonation}
+							deleteDonation={deleteDonation}
+						/>
 						{openDonationModal && (
 							<EditDonationModal
 								open={openDonationModal}
---
+++ dashboard/src/app/(app)/postings/postings-table.tsx
@@ -0,0 +1,64 @@
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "../../components/Table";
+import { Icons } from "@/app/components/Icons";
+import convertDateToString from "@/app/utils/date";
+import { useGetAllUserDonations } from "@/app/queries/donations";
+import type { TDonation } from "@/app/utils/types";
+
+type TPostingsProps = {
+	editDonation: (donation: TDonation) => void;
+	deleteDonation: (donation: TDonation) => void;
+};
+
+export default function PostingsTable(props: TPostingsProps) {
+	const { editDonation, deleteDonation } = props;
+
+	const { data: donations } = useGetAllUserDonations();
+
+	return (
+		<Table>
+			<TableHeader>
+				<TableRow>
+					<TableHead className="w-[100px] font-semibold">Title</TableHead>
+					<TableHead className="font-semibold">Status</TableHead>
+					<TableHead className="font-semibold">Category</TableHead>
+					<TableHead className="font-semibold">Description</TableHead>
+					<TableHead className="font-semibold">Pickup Location</TableHead>
+					<TableHead className="font-semibold">Pickup Time</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				{donations?.map((donation) => {
+					return (
+						<TableRow key={donation.id}>
+							<TableCell className="font-medium">{donation.title}</TableCell>
+							<TableCell className="font-medium">{donation.status}</TableCell>
+							<TableCell className="font-medium">{donation.category}</TableCell>
+							<TableCell className="font-medium">
+								{donation.description}
+							</TableCell>
+							<TableCell className="font-medium">{donation.location}</TableCell>
+							<TableCell className="font-medium">
+								{convertDateToString(donation.time)}
+							</TableCell>
+							<TableCell className="font-medium">
+								<span className="flex flex-row">
+									{donation.status !== "RETRIEVED" && (
+										<Icons.PencilIcon onClick={() => editDonation(donation)} />
+									)}
+									<Icons.TrashIcon onClick={() => deleteDonation(donation)} />
+								</span>
+							</TableCell>
+						</TableRow>
+					);
+				})}
+			</TableBody>
+		</Table>
+	);
+}
